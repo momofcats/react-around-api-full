bacecdc2ac94bf4f8d050a1c9ab8b4bc891fada0
Yuliya Khilko <ykhilko@gmail.com>
2020-09-07 15:38:44 -0700

    feat: serve static files

---
+++ public/precache-manifest.c14e856184f0c5cf0a232ebb7118e84b.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ac811192aa2646d7648fff7cbd866b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9dc31e5331f634bd7f1b",
+    "url": "/static/css/main.c074dffc.chunk.css"
+  },
+  {
+    "revision": "50b417e0c3b4c768d3e3",
+    "url": "/static/js/2.c1df13c5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c1df13c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9dc31e5331f634bd7f1b",
+    "url": "/static/js/main.d226aa7f.chunk.js"
+  },
+  {
+    "revision": "076524b2c5b339f12c41",
+    "url": "/static/js/runtime-main.f1369426.js"
+  },
+  {
+    "revision": "e37354838bf8078ac0d296217613533f",
+    "url": "/static/media/Inter-Black.e3735483.woff"
+  },
+  {
+    "revision": "79260e5b693fac2ee373b659f01a2bdf",
+    "url": "/static/media/Inter-Bold.79260e5b.woff"
+  },
+  {
+    "revision": "7a8cc7241f766a142e15b2948804e547",
+    "url": "/static/media/Inter-Medium.7a8cc724.woff"
+  },
+  {
+    "revision": "7c539936c4c8c822b59a1bcc6c08a6ec",
+    "url": "/static/media/Inter-Regular.7c539936.woff"
+  },
+  {
+    "revision": "5df32a06fdf69f3ae19600017c36d45a",
+    "url": "/static/media/close_icon.5df32a06.svg"
+  },
+  {
+    "revision": "f202ad3053ae4410e6d246994df87581",
+    "url": "/static/media/del-icon.f202ad30.svg"
+  },
+  {
+    "revision": "b9b65c7e7e563b8df5ac93cc01872a72",
+    "url": "/static/media/edit.b9b65c7e.svg"
+  },
+  {
+    "revision": "3fc5b7b0be63876c31af74691848fbd6",
+    "url": "/static/media/heart.3fc5b7b0.svg"
+  },
+  {
+    "revision": "5bbb05f725aa334a73acf3174e255f4e",
+    "url": "/static/media/like-dark.5bbb05f7.svg"
+  },
+  {
+    "revision": "8f7611ae39c682f37516feec261f77dd",
+    "url": "/static/media/logo.8f7611ae.svg"
+  },
+  {
+    "revision": "50cf486163827b9b62c764268a435184",
+    "url": "/static/media/plus.50cf4861.svg"
+  }
+]);
