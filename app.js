7f20d0a5804d6a148875cbd523f7e10683e5422c
Yuliya Khilko <ykhilko@gmail.com>
2020-09-08 22:27:49 -0700

    feat: load cards and user

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const path = require('path');
 const userRouter = require('./routes/users');
+const cardsRouter = require('./routes/cards');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/', userRouter);
+app.use('/users', userRouter);
+app.use('/cards', cardsRouter);
+app.get('*', (req, res) => res.status(404).send({ message: 'Requested resource not found' }));
 app.listen(PORT, () => {
   console.log(`App listening at port ${PORT}`);
 });
--- routes/users.js
+++ routes/users.js
@@ -7,11 +7,22 @@ const fs = require('fs').promises;
 const dataPath = path.join(__dirname, '..', 'data', 'users.json');
 
 const getDataFromFile = (pathToFile) => fs.readFile(pathToFile, { encoding: 'utf8' })
-  .then((data) => JSON.parse(data))
-  .catch((err) => console.log(err));
+  .then((data) => JSON.parse(data));
 
-const getUsers = (req, res) => getDataFromFile(dataPath).then((users) => res.send(users));
+const getUsers = (req, res) => getDataFromFile(dataPath)
+  .then((users) => res.status(200).send(users))
+  .catch(() => res.status(404).send({ message: 'Requested resource not found' }));
 
-userRouter.get('/users', getUsers);
+const getUser = (req, res) => getDataFromFile(dataPath)
+  .then((users) => users.find((user) => user._id === req.params.id))
+  .then((user) => {
+    if (user) {
+      res.status(200).send(user);
+    }
+    res.status(404).send({ message: 'User ID not found' });
+  })
+  .catch((err) => res.status(400).send(err));
 
+userRouter.get('/', getUsers);
+userRouter.get('/:id', getUser);
 module.exports = userRouter;
