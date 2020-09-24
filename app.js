63a0ea577242b93b0deed0574710a516552a5d8c
Yuliya Khilko <ykhilko@gmail.com>
2020-09-23 19:59:09 -0700

    feat: remove public and data

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const path = require('path');
 
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
@@ -27,7 +26,6 @@ mongoose.connect('mongodb://localhost:27017/aroundb', {
 });
 
 app.use(jsonParser);
-app.use(express.static(path.join(__dirname, 'public')));
 app.use('/users', userRouter);
 app.use('/cards', cardsRouter);
 app.use((req, res) => {
--- helpers/getDataFromFile.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs').promises;
-
-const getDataFromFile = (pathToFile) => fs.readFile(pathToFile, { encoding: 'utf8' })
-  .then((data) => JSON.parse(data));
-
-module.exports = getDataFromFile;
