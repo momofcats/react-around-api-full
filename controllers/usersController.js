80f234e2e3901d35199eb8ffbdf695f1a3606d0b
Yuliya Khilko <ykhilko@gmail.com>
2020-10-19 20:57:17 -0700

    add email and hashed password to add new user

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcryptjs');
 const User = require('../models/user');
 const {
   STATUS_CODE_BAD_REQUEST,
@@ -34,11 +35,18 @@ const getUser = (req, res) => {
 };
 
 const addUser = (req, res) => {
-  const { name, about, avatar } = req.body;
-  User.create({ name, about, avatar })
+  bcrypt.hash(req.body.password, 10)
+    .then((hash) => User.create({
+      name: req.body.name,
+      about: req.body.about,
+      avatar: req.body.avatar,
+      email: req.body.email,
+      password: hash,
+    }))
     .then((user) => {
       res.status(STATUS_CODE_OK).send({ data: user });
-    }).catch((err) => {
+    })
+    .catch((err) => {
       if (err.name === 'ValidationError') {
         return res.status(STATUS_CODE_BAD_REQUEST).send({ message: err.message });
       }
--- models/user.js
+++ models/user.js
@@ -1,20 +1,21 @@
 const mongoose = require('mongoose');
 const isEmail = require('validator/lib/isEmail');
+const isURL = require('validator/lib/isURL');
 
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
     validate: {
-      validator: isEmail,
-      message: 'is not valid email',
-      //isAsync: false,
+      validator: (v) => isEmail(v),
+      message: 'Wrong email format',
     },
     unique: true,
   },
   password: {
     type: String,
     required: true,
+    minlength: 8,
   },
   name: {
     type: String,
@@ -32,13 +33,11 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
     validate: {
-      validator(v) {
-        const regExp = /^https?:\/\/(www\.)?[\w/,-]+\.[\w/-]+#*$/;
-        return regExp.test(v);
-      },
+      validator: (v) => isURL(v),
       message: 'Invalid Url',
     },
   },
+
 }, { versionKey: false });
 
 module.exports = mongoose.model('user', userSchema);
