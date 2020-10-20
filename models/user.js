5964bbb1d302227b7902fd8b69eaac660e7e873f
Yuliya Khilko <ykhilko@gmail.com>
2020-10-19 19:16:23 -0700

    add email and password to user

--- models/user.js
+++ models/user.js
@@ -1,6 +1,21 @@
 const mongoose = require('mongoose');
+const isEmail = require('validator/lib/isEmail');
 
 const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    validate: {
+      validator: isEmail,
+      message: 'is not valid email',
+      //isAsync: false,
+    },
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
   name: {
     type: String,
     required: true,
