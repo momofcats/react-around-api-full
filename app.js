053b56c897d17e8fde02ba94ce1a69fdfeb58770
Yuliya Khilko <ykhilko@gmail.com>
2020-10-22 12:01:38 -0700

    create error classes and and handle errors in app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const userRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
 const { loginUser, addUser } = require('./controllers/usersController');
+const STATUS_CODE_INTERNAL_SERVER_ERROR = require('./utils/statusCodes');
 
 const jsonParser = bodyParser.json();
 
@@ -24,8 +25,13 @@ app.post('/signup', addUser);
 app.post('/signin', loginUser);
 app.use('/users', userRouter);
 app.use('/cards', cardsRouter);
-app.use((req, res) => {
-  res.status(404).send({ message: 'Requested resource not found' });
+app.use((err, req, res, next) => {
+  const { statusCode = STATUS_CODE_INTERNAL_SERVER_ERROR, message } = err;
+  res.status(statusCode).send({
+    message: statusCode === STATUS_CODE_INTERNAL_SERVER_ERROR
+      ? 'an error occured on the server'
+      : message,
+  });
 });
 app.listen(PORT, () => {
   console.log(`App listening at port ${PORT}`);
