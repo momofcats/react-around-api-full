47a20023960d878a18eac7c48123928754db51b5
Yuliya Khilko <ykhilko@gmail.com>
2020-10-27 16:08:48 -0700

    make only email and password required fields

--- app.js
+++ app.js
@@ -27,9 +27,9 @@ app.use(requestLogger);
 
 app.post('/signup', celebrate({
   body: Joi.object().keys({
-    name: Joi.string().required().min(2).max(30),
-    about: Joi.string().min(2).max(30).required(),
-    avatar: Joi.string().required().uri(),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().uri(),
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
   }),
