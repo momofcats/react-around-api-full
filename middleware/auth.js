b712acca2062aea9e1825d852c4241c4fc950a7c
Yuliya Khilko <ykhilko@gmail.com>
2020-10-26 19:06:23 -0700

    add errors logger and auth

---
+++ middleware/auth.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+const UnauthorizedError = require('../errors/unauthorized-err');
+
+module.exports = (req, res, next) => {
+  const { authorization } = req.headers;
+  if (!authorization || !authorization.startsWith('Bearer')) {
+    throw new UnauthorizedError('Authorization required');
+  }
+  const token = authorization.replace('Bearer', '');
+  let payload;
+  try {
+    payload = jwt.verify(token, 'super-strong-secret');
+  } catch (err) {
+    throw new UnauthorizedError('Authorization required');
+  }
+  req.user = payload;
+  next();
+};
