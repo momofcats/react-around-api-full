07e15e58d9b0ee041b5a971fda0ba9cb9c78f990
Yuliya Khilko <ykhilko@gmail.com>
2020-10-27 13:01:19 -0700

    remove joi header authorization

--- routes/cards.js
+++ routes/cards.js
@@ -3,9 +3,7 @@ const { celebrate, Joi } = require('celebrate');
 const { getCards, createCard, deleteCard } = require('../controllers/cardsController');
 const auth = require('../middleware/auth');
 
-cardsRouter.get('/', auth, celebrate({
-  authorization: Joi.required().string(),
-}), getCards);
+cardsRouter.get('/', auth, getCards);
 cardsRouter.post('/', auth, celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
