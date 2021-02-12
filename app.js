b751ea88cd2c56e3d0ffe0830139ff9c9906856d
Yuliya Khilko <ykhilko@gmail.com>
2021-02-11 16:29:36 -0800

    add local db

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const errorHandler = require('./middleware/errorHandler');
 
 const jsonParser = bodyParser.json();
 
-const { PORT = 3000, MONGODB_URI, NODE_ENV } = process.env;
+const { PORT = 3000, MONGODB_URI = 'mongodb://localhost:27017/aroundb', NODE_ENV } = process.env;
 const NotFoundError = require('./errors/not-found-err');
 const errorMessages = require('./utils/errorMessages');
 
